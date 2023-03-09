7d151c1d72652d039d5ad6aaeaaf278b541757e9
KODA97 <acovamartinez@gmail.com>
2023-03-08 19:16:11 -0600

    Xd
    XD

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect, request, abort
+import requests
 
 app = Flask(__name__)
 
@@ -7,9 +8,29 @@ url = 'https://api-flask-6939.onrender.com/api/alumnos'
 app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        return render_template('index.html')
+        alumnos = request.get(url).json()['Alumnos']
+        completed = []
+        incompleted = []
+        for task in task:
+            if task['status'] == True:
+                completed.append(task)
+            else:
+                incompleted.append(task)
+        print(f'COMPLETAS: {completed}')
+        print(f'INCOMPLETAS: {incompleted}')
+        #print(alumnos)
+        response = {'completed': completed,
+                    'incompleted': incompleted,
+                    'counter1': len(completed),
+                    'counter2': len(incompleted)}
+        return render_template('index.html', response = response)
     else:
-        pass
+        name = request.form['name']
+        try:
+            requests.post(url, json={'name': name})
+            return redirect('/')
+        except:
+            return abort(500)
 
 if __name__ == "__mian__":
     app.run(debug = True)
