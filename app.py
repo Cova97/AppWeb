184776019a35ec82aee2f8b70f4e8eccef8c8a9a
KODA97 <acovamartinez@gmail.com>
2023-03-14 16:56:29 -0600

    taks
    ya se pueden agregar nuevas task

--- app.py
+++ app.py
@@ -9,43 +9,32 @@ url = 'https://api-flask-g929.onrender.com/items'
 def home():
     if request.method == 'GET':
         tasks = requests.get(url).json()
-        print(tasks)
-        response = {'tasks': tasks}
-        return render_template('index.html', response = response)
-    else:
-        abort(505)
-
-if __name__ == "__main__":
-    app.run(debug = True)
-
-"""
-
-
         completed = []
-        incompleted = []
-        for item in item:
-            if item['status'] == True:
-                completed.append(item)
+        incompleted  = []
+        for task in tasks:
+            if task['status'] == True:
+                completed.append(task)
             else:
-                incompleted.append(item)
-        print(f'COMPLETAS: {completed}')
-        print(f'INCOMPLETAS: {incompleted}')
-        
+                incompleted.append(task)
+        print(f'Tareas Completas: {completed}')
+        print(f'Tareas Incompletas: {incompleted}')
+        #print(tasks)
+        #response = {'tasks': tasks}
         response = {'completed': completed,
                     'incompleted': incompleted,
                     'counter1': len(completed),
                     'counter2': len(incompleted)}
         return render_template('index.html', response = response)
-
-
-"""
-
-"""
-name = request.form['name']
+    else:
+        name = request.form['name']
         try:
             requests.post(url, json={'name': name})
             return redirect('/')
         except:
             return abort(500)
 
-"""
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
+
